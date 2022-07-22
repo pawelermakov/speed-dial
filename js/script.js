fd39b164123c592f711010d8c96bec4e6a21f146
Pavel Ermakov <pawelermakov@gmail.com>
2022-07-22 22:06:06 +0300

    Add search

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ class SpeedDial {
     this.getAll()
     this.initModals()
     this.initContextMenu()
+    this.initSearch()
 	}
 
   getAll() {
@@ -151,11 +152,11 @@ class SpeedDial {
   }
 
   initModals() {
-    const modal    = document.querySelectorAll('.speed-dial__modal')
-    const close    = document.querySelectorAll('.speed-dial__modal-close')
-    const formAdd  = document.querySelector('.speed-dial__modal-add form')
+    const modal = document.querySelectorAll('.speed-dial__modal')
+    const close = document.querySelectorAll('.speed-dial__modal-close')
+    const formAdd = document.querySelector('.speed-dial__modal-add form')
     const formEdit = document.querySelector('.speed-dial__modal-edit form')
-    const that     = this
+    const that = this
 
     formAdd.addEventListener('submit', (e) => {
       const url = formAdd.querySelector('[name="url"]').value
@@ -170,8 +171,8 @@ class SpeedDial {
     })
 
     formEdit.addEventListener('submit', (e) => {
-      const id    = formEdit.querySelector('[name="id"]').value
-      const url   = formEdit.querySelector('[name="url"]').value
+      const id = formEdit.querySelector('[name="id"]').value
+      const url = formEdit.querySelector('[name="url"]').value
       const title = formEdit.querySelector('[name="title"]').value
 
       that.edit(id, url, title)
@@ -193,7 +194,7 @@ class SpeedDial {
 
     document.addEventListener('click', (e) => {
       modal.forEach((el) => {
-        if(e.target == el) {
+        if(e.target === el) {
           this.hide(el)
         }
       })
@@ -202,20 +203,25 @@ class SpeedDial {
 
   initContextMenu() {
     let tab = null
-    let id  = null
+    let id = null
 
     document.addEventListener('contextmenu', async(e) => {
-      e.preventDefault()
-
       const tabItem = e.target.closest('.speed-dial__tabs-item')
-      let context   = document.querySelector('.speed-dial__context-menu-page')
+      const search = e.target.closest('.speed-dial__search-input')
+      let context = document.querySelector('.speed-dial__context-menu-page')
+
+      if(search) {
+        return false
+      }
+
+      e.preventDefault()
 
       if(tabItem) {
         context = document.querySelector('.speed-dial__context-menu-tab')
-        id      = tabItem.getAttribute('data-id')
+        id = tabItem.getAttribute('data-id')
       }
 
-      context.style.top  = (e.clientY + context.clientHeight >= screen.height) ? `${e.clientY - context.clientHeight}px` : `${e.clientY}px`
+      context.style.top = (e.clientY + context.clientHeight >= screen.height) ? `${e.clientY - context.clientHeight}px` : `${e.clientY}px`
       context.style.left = (e.clientX + context.clientWidth >= screen.width) ? `${e.clientX - context.clientWidth}px` : `${e.clientX}px`
       this.show(context)
 
@@ -243,9 +249,9 @@ class SpeedDial {
           break
 
         case 'edit':
-          const formEdit                                 = document.querySelector('.speed-dial__modal-edit form')
-          formEdit.querySelector('[name="id"]').value    = tab.id
-          formEdit.querySelector('[name="url"]').value   = tab.url
+          const formEdit = document.querySelector('.speed-dial__modal-edit form')
+          formEdit.querySelector('[name="id"]').value = tab.id
+          formEdit.querySelector('[name="url"]').value = tab.url
           formEdit.querySelector('[name="title"]').value = tab.title
           this.show(document.querySelector('.speed-dial__modal-edit'))
           break
@@ -265,18 +271,18 @@ class SpeedDial {
   initSorting() {
     const dropZones = document.querySelectorAll('.speed-dial__tabs')
     const dragItems = document.querySelectorAll('.speed-dial__tabs-item')
-    const newTab    = document.querySelector('.speed-dial__tabs-item-new')
+    const newTab = document.querySelector('.speed-dial__tabs-item-new')
     let draggedItem = null
     let droppedItem = null
 
     dragItems.forEach((dragItem) => {
       dragItem.addEventListener('dragstart', () => {
-        draggedItem          = dragItem
+        draggedItem = dragItem
         newTab.style.display = 'none'
       })
 
       dragItem.addEventListener('dragend', () => {
-        draggedItem          = null
+        draggedItem = null
         newTab.style.display = 'block'
       })
 
@@ -301,29 +307,125 @@ class SpeedDial {
       dropZone.addEventListener('drop', () => {
         if(droppedItem) {
           if(droppedItem.parentElement === draggedItem.parentElement) {
-            const children     = Array.from(droppedItem.parentElement.children)
+            const children = Array.from(droppedItem.parentElement.children)
             const draggedIndex = children.indexOf(draggedItem)
             const droppedIndex = children.indexOf(droppedItem)
 
             if(draggedIndex > droppedIndex) {
               draggedItem.parentElement.insertBefore(draggedItem, droppedItem)
-            }
-            else {
+            } else {
               draggedItem.parentElement.insertBefore(draggedItem, droppedItem.nextElementSibling)
             }
-          }
-          else {
+          } else {
             dropZone.insertBefore(draggedItem, droppedItem)
           }
 
           document.querySelectorAll('.speed-dial__tabs-item').forEach((tab, index) => {
-            const id       = tab.getAttribute('data-id')
+            const id = tab.getAttribute('data-id')
             const position = index + 1
 
             this.setPosition(id, position)
           })
         }
       })
+    })
+  }
+
+  initSearch() {
+	  const form = document.querySelector('.speed-dial__search')
+    const input = document.querySelector('.speed-dial__search-input')
+    const button = document.querySelector('.speed-dial__search-button')
+    const suggestions = document.querySelector('.speed-dial__search-suggestions')
+    let suggestionsItems = ''
+    let suggestionsActive = ''
+
+    form.addEventListener('submit', (e) => {
+      if(input.value === '') {
+        e.preventDefault()
+      }
+    })
+
+    input.addEventListener('input', async () => {
+      const res = await fetch(`https://suggest.yandex.ru/suggest-sl?part=${input.value}`)
+      const data = await res.json()
+
+      button.disabled = (input.value === '')
+
+      if(data[1].length) {
+        suggestions.innerHTML = ''
+
+        data[1].forEach((el) => {
+          const item = document.createElement('div')
+
+          item.classList.add('speed-dial__search-suggestions-item')
+          item.innerHTML = el
+          suggestions.appendChild(item)
+
+          item.addEventListener('click', (e) => {
+            input.value = e.target.innerHTML
+            form.submit()
+            this.hide(suggestions)
+          })
+
+          item.addEventListener('mouseenter', (e) => {
+            suggestionsItems.forEach((el) => {
+              el.classList.remove('speed-dial__search-suggestions-item--active')
+            })
+
+            e.target.classList.add('speed-dial__search-suggestions-item--active')
+            suggestionsActive = e.target
+          })
+
+          item.addEventListener('mouseleave', () => {
+            suggestionsItems.forEach((el) => {
+              el.classList.remove('speed-dial__search-suggestions-item--active')
+            })
+
+            suggestionsActive = ''
+          })
+        })
+
+        suggestionsItems = document.querySelectorAll('.speed-dial__search-suggestions-item')
+        this.show(suggestions)
+      }
+    })
+
+    input.addEventListener('focus', () => {
+      if(input.value !== '') {
+        this.show(suggestions)
+      }
+    })
+
+    input.addEventListener('blur', () => {
+      this.hide(suggestions)
+    })
+
+    document.addEventListener('keyup', (e) => {
+      if(getComputedStyle(suggestions).display === 'block') {
+        if(e.key === 'ArrowUp') {
+          if(!suggestionsActive) {
+            suggestionsActive = suggestions.lastChild
+          } else {
+            suggestionsActive.classList.remove('speed-dial__search-suggestions-item--active')
+            suggestionsActive = (suggestionsActive.previousElementSibling) ? suggestionsActive.previousElementSibling : suggestions.lastChild
+          }
+
+          suggestionsActive.classList.add('speed-dial__search-suggestions-item--active')
+          input.value = suggestionsActive.innerHTML
+        }
+
+        if(e.key === 'ArrowDown') {
+          if(!suggestionsActive) {
+            suggestionsActive = suggestions.firstChild
+          } else {
+            suggestionsActive.classList.remove('speed-dial__search-suggestions-item--active')
+            suggestionsActive = (suggestionsActive.nextElementSibling) ? suggestionsActive.nextElementSibling : suggestions.firstChild
+          }
+
+          suggestionsActive.classList.add('speed-dial__search-suggestions-item--active')
+          input.value = suggestionsActive.innerHTML
+        }
+      }
     })
   }
 }
